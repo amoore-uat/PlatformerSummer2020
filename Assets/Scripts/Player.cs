0f0d44d19e70ce9542050d2b5609defd3699c428
Adam Moore <amoore@uat.edu>
2020-07-15 13:33:51 -0700

    Animation lecture. Has minor bugs. We'll debug later.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,24 +6,40 @@ public class Player : MonoBehaviour
 {
     public float speed = 3f;
     public float jumpForce = 5f;
+    public int currentJumps;
     public int maxJumps = 1;
     public float height = 1.1f;
 
     private Rigidbody2D rigidBody;
     private SpriteRenderer sprite;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        currentJumps = maxJumps;
     }
 
     // Update is called once per frame
     void Update()
     {
         float xMovement = Input.GetAxis("Horizontal") * speed;
-
+        animator.SetFloat("xMove", Mathf.Abs(xMovement));
         rigidBody.velocity = new Vector2(xMovement, rigidBody.velocity.y);
+
+        // Handling animations through code instead of the animation system
+        /*
+        if (rigidBody.velocity.x != 0)
+        {
+            animator.Play("PlayerWalk");
+        }
+        else
+        {
+            animator.Play("PlayerIdle");
+        }
+        */
 
         if (rigidBody.velocity.x > 0)
         {
@@ -36,18 +52,35 @@ public class Player : MonoBehaviour
 
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            if (IsGrounded())
+            {
+                currentJumps = maxJumps;
+            }
+            if (currentJumps > 0)
+            {
+                Jump();
+            }
         }
     }
 
     void Jump()
     {
-        rigidBody.AddForce(Vector2.up * jumpForce);
+        currentJumps--;
+        animator.SetTrigger("jump");
+        //rigidBody.AddForce(Vector2.up * jumpForce);
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
     }
 
     bool IsGrounded()
     {
         RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, Vector2.down, (height / 2f) + 0.1f);
-        return (hitinfo.collider != null);
+        bool grounded = (hitinfo.collider != null);
+        animator.SetBool("isGrounded", grounded);
+        return grounded;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        IsGrounded();
     }
 }
