e8a7f575ad145c7cda6eefff673023f1476187a1
Adam Moore <amoore@uat.edu>
2020-07-20 13:25:16 -0700

    Animation bugs addressed. Scene Management implemented.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager instance;
+    public int currentSceneIndex = 0;
+
+    public GameObject playerPrefab;
+    public GameObject player;
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Debug.LogError("[GameManager] Attempted to create a second instance of GameManager");
+            Destroy(this);
+        }
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            LoadNextScene();
+        }
     }
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    /// <summary>
+    /// This method is called every time a scene finishes loading.
+    /// </summary>
+    /// <param name="scene"></param>
+    /// <param name="mode"></param>
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Scene finished loading");
+        currentSceneIndex = scene.buildIndex;
+    }
+
+    public void LoadNextScene()
+    {
+        LoadLevel(currentSceneIndex + 1);
+    }
+
 }
